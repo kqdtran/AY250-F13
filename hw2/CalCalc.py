8e9cc83c7621f15ef48a0bf1b3adaa07acd07dc8
Khoa Tran <khoatran@berkeley.edu>
2013-09-16 00:41:37 -0700

    Fix Unicode encoding issues...

--- hw2/CalCalc.py
+++ hw2/CalCalc.py
@@ -13,30 +13,34 @@ def buildParser():
     results = parser.parse_args()
     return results
 
-def evaluate(stringExp):
+def calculate(stringExp):
     """Use the evil 'eval' in a clean namespace to prevent injection.
     Suggested on http://stackoverflow.com/a/2372145/2465041
 
     stringExp - the string expression to be evaluated
                 if exception, send to Wolfram Alpha"""
     
-    ns = {'__builtins__': None}
     try:
+        ns = {'__builtins__': None}
         result = eval(stringExp, ns)
     except SyntaxError: # Wolfram Alpha helps meeee
         print "Querying Wolfram Alpha... Please wait..."
         url = "http://api.wolframalpha.com/v2/query"
         values = {'input': stringExp, 'format': 'plaintext', 'appid': 'UAGAWR-3X6Y8W777Q'}
         data = urllib.urlencode(values)
+
+        # After encoding the parameters, send a request
         req = urllib2.Request(url, data)
         response = urllib2.urlopen(req)
         xmlDoc = response.read()
+
+        # Use BeautifulSoup to parse XML and retrieve the result from Wolfram Alpha
         soup = Soup(xmlDoc)
         resultList = soup.find_all('plaintext')
-        result = resultList[1].string
+        result = resultList[1].string.encode("ascii", errors='ignore').strip()
     return result
     
 if __name__ == "__main__":
     parseOptions = buildParser()
     stringExp = parseOptions.stringExp
-    print evaluate(stringExp)
+    print calculate(stringExp)
